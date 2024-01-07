adb9e9c645487302fccc0c6b2e108c5b84cf1e5c
hagouchikarim <hagouchik@gmail.com>
2024-01-07 06:32:33 -0800

    .

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - singly linked list
+ * @head: head list
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL )
+		return (1);
+	return (alx_palind(head, *head));
+}
+
+/**
+ * is_palindrome - singly linked list
+ * @head: head list
+ * @end: end list
+ */
+int alx_palind(listint_t **head, listint_t *end)
+{
+		if (end == NULL)
+			return (1);
+		if (alx_palind(head, end->next) && (*head)->n == end->n)
+		{
+			*head = (*head)->next;
+			return (1);
+		}
+		return (0);
+}
+
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This Is Readmefile for python data structures: lists, tuples
